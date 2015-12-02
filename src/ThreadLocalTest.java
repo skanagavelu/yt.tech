aab0e470c64d048356469d6d0f4a19fc23fafd21
ksugumar <ksugumar@INLD50873905A.apj.global.corp.sap>
2015-12-02 16:45:34 +0530

    Initial

---
+++ src/ThreadLocalTest.java
@@ -0,0 +1,85 @@
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+
+public class ThreadLocalTest {
+
+    public static void main(String args[]) throws IOException {
+        Thread firstThread = new Thread(new Job());   
+        Thread secondThread = new Thread( new Job());
+        firstThread.start();
+        secondThread.start();       
+    }
+    
+    public static DateFormat getThreadSafeDateFormater(){
+        DateFormat formatter = SimpleDateFormatInstancePerThread.getDateFormatter();
+        return formatter;
+    }
+    
+    /*
+     * SimpleDateFormat is the immutable class and shared resource which has to be synchronized
+     * while performing format method call.   
+     * So better give new instance of SimpleDateFormat for each thread, so that 
+     * format method will be called on only one thread which owns SimpleDateFormat instance. 
+     */
+    public static String threadSafeFormat(DateFormat formatter, Date date){
+        return formatter.format(date);
+    }
+    
+    
+    public static void cleanUpThreadSafeDateFormater(){
+        SimpleDateFormatInstancePerThread.cleanup();
+    }
+    
+}
+
+
+/*
+ * Thread Safe implementation of SimpleDateFormat
+ * Each Thread will get its own instance of SimpleDateFormat.
+ */
+class SimpleDateFormatInstancePerThread {
+
+    private static final ThreadLocal<SimpleDateFormat> dateFormatHolder = new ThreadLocal<SimpleDateFormat>() {
+
+        @Override
+        protected SimpleDateFormat initialValue() {
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd") {
+				UUID id = UUID.randomUUID();
+				@Override
+				public String toString() {
+					return id.toString();
+				};
+			};
+            System.out.println("Creating SimpleDateFormat instance " + dateFormat +" for Thread : " + Thread.currentThread().getName());
+            return dateFormat;
+        }
+    };
+
+    /*
+     * Every time there is a call for DateFormat, ThreadLocal will return calling
+     * Thread's copy of SimpleDateFormat
+     */
+    public static DateFormat getDateFormatter() {
+        return dateFormatHolder.get();
+    }
+    
+    public static void cleanup() {
+        dateFormatHolder.remove();
+    }
+}
+
+class Job implements java.lang.Runnable{
+    
+    @Override
+    public void run() {
+            System.out.println("Thread Name: " + Thread.currentThread().getName() + "  Formatted time: " + ThreadLocalTest.threadSafeFormat(ThreadLocalTest.getThreadSafeDateFormater(), new Date()));
+            System.out.println("Thread Name: " + Thread.currentThread().getName() + "  Date Formatter Object: " + ThreadLocalTest.getThreadSafeDateFormater() + " is cleaned.");
+            ThreadLocalTest.cleanUpThreadSafeDateFormater();
+            ThreadLocalTest.getThreadSafeDateFormater();
+    }
+}
+
